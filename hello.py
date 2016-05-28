40c29f2bf8353459dc8bb64e3409b198e80e8714
vanstinger <vandykfernandes@hotmail.com>
2016-05-28 17:00:44 +0400

    Flask variable rules

--- hello.py
+++ hello.py
@@ -10,6 +10,20 @@ def index():
     return "Hello World"
 # add_url_rule function is also available instead of using decorator
 
+# Variable rules in flask
+
+
+@app.route('/user/<username>')
+def user_profile(username):
+    return "User: %s" % (username)
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return "Post: %d" % (post_id)
+
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.debug = True
+    # app.run(host = '0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
