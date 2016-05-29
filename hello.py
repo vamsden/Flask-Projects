5f0949ff8a5b8d78950c7844c539578ee3f2f9b8
vanstinger <vandykfernandes@hotmail.com>
2016-05-30 00:06:33 +0400

    Using templates in the flask program. Corrections made to the previous commit

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # import Flask
-import requests
-from flask import Flask,
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 # Specify the route for the app and define the app view
@@ -24,24 +24,28 @@ def show_post(post_id):
     return "Post: %d" % (post_id)
 
 
-@app.route('/projects/')
-def projects():
-    return "The projects page"
+@app.route('/show_the_login_form/')
+def show_the_login_form():
+    return "The login page"
 
-# url_for('static', filename='style.css')
+
+@app.route('/do_the_login')
+def do_the_login():
+    return "The logged in page"
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        do_the_login()
+    if request.method == 'GET':
+        return "Return login page"
     else:
-        show_the_login_form()
+        return "Logged in page"
 
 
-@app.route('/about')
-def about():
-    return "The about page"
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 # Run the app
 if __name__ == '__main__':
