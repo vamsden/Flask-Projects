c7fab01a2f91442d6d70b030650fee005ef24647
vanstinger <vandykfernandes@hotmail.com>
2016-05-29 23:22:18 +0400

    GET and POST methods in flask

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 # import Flask
-from flask import Flask
+import requests
+from flask import Flask,
 app = Flask(__name__)
 
 # Specify the route for the app and define the app view
@@ -21,6 +22,26 @@ def user_profile(username):
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     return "Post: %d" % (post_id)
+
+
+@app.route('/projects/')
+def projects():
+    return "The projects page"
+
+# url_for('static', filename='style.css')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        do_the_login()
+    else:
+        show_the_login_form()
+
+
+@app.route('/about')
+def about():
+    return "The about page"
 
 # Run the app
 if __name__ == '__main__':
